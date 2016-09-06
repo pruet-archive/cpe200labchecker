d61594544eb60b12017e2e973801e9bdac3830de
pruet <pruetboonma@gmail.com>
2016-09-06 22:31:17 +0700

    fix problem with week3-lab/lab3-2

--- grading.py
+++ grading.py
@@ -1,16 +1,18 @@
 #!/bin/env python3
 
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, redirect, render_template, request, Response
 from unitchecker import UnitChecker
 from pprint import pformat
-from flask import Response
 
 app = Flask(__name__)
 app.debug = True
 
-#def stream_template(template_name, **context):
+def stream_template(template_name, **context):
+    app.update_template_context(context)
+    t = app.jinja_env.get_template(template_name)
+    rv = t.stream(context)
+    rv.enable_buffering(5)
+    return rv
 
 @app.route('/')
 def index():
@@ -18,14 +20,20 @@ def index():
 
 @app.route('/test', methods=['POST'])
 def test():
-  if request.form['password'] == 'helloworld!':
+  org = request.form['org']
+  repo = request.form['repo']
+  password = request.form['password']
+  if password != None and org != None and repo != None and org != '' and repo != '' and request.form['password'] == 'helloworld!':
     uc = UnitChecker()
-    org = request.form['org']
-    repo = request.form['repo']
-    results = uc.process_git(org, repo)
-    return render_template('results.html', results=results, org=org, repo=repo)
+    prs = uc.process_git(org, repo)
+    def generator():
+      for pr in prs:
+        result = uc.process_pullrequest(pr, repo)
+        yield result
+    #return render_template('results.html', results=results, org=org, repo=repo)
+    return Response(stream_template('results.html', results=generator(), org=org, repo=repo))
   else:
-    return 'you shouldn\'t be here'
+    return redirect('http://www.nyan.cat/', code=302)
 
-#if __name__ == "__main__":
-#  app.run(debug=True)
+if __name__ == "__main__":
+  app.run(debug=True)
--- unitchecker.py
+++ unitchecker.py
@@ -12,7 +12,8 @@ from pprint import pprint
 class UnitChecker:
   config = configparser.ConfigParser()
   def __init__(self):
-    self.config.read('/var/www/grading/config.ini')
+    #self.config.read('/var/www/grading/config.ini')
+    self.config.read('config.ini')
 
   def join(self, path1, path2):
     return os.path.join(path1, path2)
@@ -53,8 +54,49 @@ class UnitChecker:
     junit = str(self.config['java']['junit_lib'])
     java_path = str(self.config['java']['java_bin_path'])
     command = [self.join(java_path,'java') , '-cp', path + ':' + junit, 'org.junit.runner.JUnitCore',java_class]
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=FNULL, shell=False)
-    return(process.communicate()[0])
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=FNULL, shell=False)
+    proc.wait()
+    return(proc.communicate()[0])
+
+  def process_pullrequest(self, pr, repo):
+    result = {}
+    result['title'] = pr.title
+    result['git_url'] = pr.head.repo.git_url
+    local_path = self.join('/tmp', str(pr.head.repo.id))
+    #id = str(pr.head.repo.id)
+    #if id != '66341762':
+    #  continue;
+    self.remove_folder(local_path)
+    Repo.clone_from(pr.head.repo.git_url, local_path)
+    result['tests'] = {}
+    if repo.endswith('lab'):
+      dir_list = self.get_lab_list(local_path)
+    else:
+      dir_list = self.get_hw_list(local_path)
+    if dir_list is None:
+      return None
+    for dir in dir_list:
+      src_dir = self.join(dir, 'src')
+      src_code_dir = self.join(src_dir, 'cpe200')
+      for java_file in self.get_file_list(src_code_dir):
+        self.compile_java(src_dir, java_file)
+      src_test_dir = self.join(src_dir, 'cpe200Test')
+      src_test_prefix = 'cpe200Test'
+      if not os.path.exists(src_test_dir):
+        src_test_dir = self.join(src_dir, 'cpe200')
+        src_test_prefix = 'cpe200'
+      for java_file in self.get_file_list(src_test_dir):
+        self.compile_java(src_dir, java_file)
+      for java_file in self.get_file_list(src_test_dir):
+        if java_file.endswith('Test.java'):
+          (head, tail) = os.path.split(java_file)
+          java_class = src_test_prefix + '.' + tail[:-5]
+          ret_list = self.run_unittest(src_dir, java_class).decode('utf-8').split('\n')
+          if ret_list[4].startswith('OK'):
+            result['tests'][java_class] = 'OK'
+          else:
+            result['tests'][java_class] = 'FAIL'
+    return(result)
 
   def process_git(self, org, repo):
     gitUser = str(self.config['authen']['user'])
@@ -63,41 +105,13 @@ class UnitChecker:
     gh = Github(login_or_token=gitToken)
 
     prs = gh.get_organization(org).get_repo(repo).get_pulls()
-    results = []
-    for pr in prs:
-      result = {}
-      result['title'] = pr.title
-      result['git_url'] = pr.head.repo.git_url
-      local_path = self.join('/tmp', str(pr.head.repo.id))
-      #id = str(pr.head.repo.id)
-      #if id != '66341762':
-      #  continue;
-      self.remove_folder(local_path)
-      Repo.clone_from(pr.head.repo.git_url, local_path)
-      result['tests'] = {}
-      if repo.endswith('lab'):
-        dir_list = self.get_lab_list(local_path)
-      else:
-        dir_list = self.get_hw_list(local_path)
-      if dir_list is None:
-        return None
-      for dir in dir_list:
-        src_dir = self.join(dir, 'src')
-        for java_file in self.get_file_list(src_dir):
-          self.compile_java(src_dir, java_file)
-        for java_file in self.get_file_list(src_dir):
-          if java_file.endswith('Test.java'):
-            (head, tail) = os.path.split(java_file)
-            java_class = tail[:-5]
-            ret_list = self.run_unittest(src_dir, java_class).decode('utf-8').split('\n')
-            if ret_list[4].startswith('OK'):
-              result['tests'][java_class] = 'OK'
-            else:
-              result['tests'][java_class] = 'FAIL'
-      results.append(result)
-    return(results)
+    return prs
+
 #uc = UnitChecker()
 #org = 'cpe200-159-sec1'
-#repo = 'cpe200-week2-lab'
-#result = uc.process_git(org, repo)
-#pprint(result)
+#repo = 'cpe200-week3-lab'
+#prs = uc.process_git(org, repo)
+#for pr in prs:
+#  result = uc.process_pullrequest(pr, repo)
+#  pprint(result)
+#  break
